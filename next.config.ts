37ab1105301a1b183a20245b6e9bdfac6411bbbe
jet45 <jet45120@gmail.com>
2026-09-04 11:55:19 +0200

    Deploy site to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: process.env.GITHUB_ACTIONS ? "/Hlist" : "",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
