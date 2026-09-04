e22535a59eb7b65349a2c9b5801384b6d3b6c118
jet45 <jet45120@gmail.com>
2026-09-04 11:59:30 +0200

    Make exported routes compatible with Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.GITHUB_ACTIONS ? "/Hlist" : "",
+  trailingSlash: true,
   images: {
     unoptimized: true,
     remotePatterns: [
